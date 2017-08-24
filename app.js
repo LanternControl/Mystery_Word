8720f3c8638037e27efeb434203fa4742dbf0449
LanternControl <brett4president@hotmail.com>
2017-08-24 11:43:14 -0400

    Set up framework and installed modules

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mustache = require('mustache-express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const app = express();
+
+app.engine('mustache', mustache());
+app.set('views', './views');
+app.set('view engine', 'mustache');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(session({
+ secret: 'nehhhh spoot',
+ resave: false,
+ saveUninitalized: true
+}));
+
+app.get('/', function(req, res){
+ res.render('index');
+});
+
+
+
+app.listen(port, function(req, res){
+ console.log('Starting express-session login app...');
+});
